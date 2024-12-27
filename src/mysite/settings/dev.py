ae0f94940cbdd0fdcff0afec1c4eda868ae27a7c
Brian Cohan <briancohan@gmail.com>
2024-12-26 19:29:16 -0500

    root of it all

---
+++ src/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0-@9bkdire82))xw(2w7c^7r4n-f^e^t&ni2*4p3a!-1588g_+"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
